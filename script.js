9891b8e2f133f5ca6b8993b18df09413c9aba5a9
JG <jonathan.garcia130207@gmail.com>
2024-10-26 12:31:08 -0400

    supreme-victory

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ let p = document.createElement("p");
 let pid= document.getElementById("p");
 let mensaje;
 
-btn.addEventListener("click", (e) => {
+
     for(let cont=1 ; cont<=20; cont++){
         if (cont%2 === 0){
-            mensaje = document.createTextNode(("el numero" + cont + "es parr"));
-            p.appendChild(mensaje);
+            console.log ("el numero" + cont + "es parr");
         }else{
-          mensaje = document.createTextNode(("el numero" + cont + "es imparr"));
-          p.appendChild(mensaje);
+           console.log("el numero" + cont + "es imparr");
         }
 
-        document.body.insertBefore(p, pid);
         }
-})
